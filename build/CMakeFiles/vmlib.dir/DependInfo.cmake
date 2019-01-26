d600e06b42e5b80df3ff108ecd1b3a58a4a8ea69
Andrew Bregger <adb3649@tamu.edu>
2019-01-26 02:30:16 -0600

    Initial Commit of Virtual machine

---
+++ build/CMakeFiles/vmlib.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/andy/Documents/Vm/src/chunk.cpp" "/Users/andy/Documents/Vm/build/CMakeFiles/vmlib.dir/src/chunk.cpp.o"
+  "/Users/andy/Documents/Vm/src/instruction.cpp" "/Users/andy/Documents/Vm/build/CMakeFiles/vmlib.dir/src/instruction.cpp.o"
+  "/Users/andy/Documents/Vm/src/object.cpp" "/Users/andy/Documents/Vm/build/CMakeFiles/vmlib.dir/src/object.cpp.o"
+  "/Users/andy/Documents/Vm/src/opcode.cpp" "/Users/andy/Documents/Vm/build/CMakeFiles/vmlib.dir/src/opcode.cpp.o"
+  "/Users/andy/Documents/Vm/src/value.cpp" "/Users/andy/Documents/Vm/build/CMakeFiles/vmlib.dir/src/value.cpp.o"
+  "/Users/andy/Documents/Vm/vm.cpp" "/Users/andy/Documents/Vm/build/CMakeFiles/vmlib.dir/vm.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
